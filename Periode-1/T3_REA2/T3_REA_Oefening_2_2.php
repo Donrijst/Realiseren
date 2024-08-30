b052b307dea722557d4373d51ffe35ff6db8030b
Don Kieu <2177651@talnet.nl>
2024-08-30 12:27:44 +0200

    startup
    begin

---
+++ Periode-1/T3_REA2/T3_REA_Oefening_2_2.php
@@ -0,0 +1,41 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>uitwerking</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="header">
+        <h1>Uitwerking van PHP-oefeningen</h1>
+    </div>
+
+    <main>
+        <div class="nav">
+            <h3>Thema 3</h3>
+
+            <ul>
+                <li>
+                    <p> Hoofdstuk 2</p>
+                    <li a href="X">Oefening 2.1</li>
+                    <li a href="x">Oefening 2.2</li>
+                </li>
+            </ul>
+        </div>
+
+        <div class="content">
+            <h3>Uitwerking</h3>
+
+            <p>Op deze plek komt de uitwerking van de oefening te staan</p>
+        </div>
+    </main>
+
+
+</body>
+</html>
