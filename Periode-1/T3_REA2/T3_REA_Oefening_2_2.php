859b9daf4f870e2107c972027de5a7a7c597d52d
Don Kieu <2177651@talnet.nl>
2024-09-05 11:41:58 +0200

    commentaar update

--- Periode-1/T3_REA2/T3_REA_Oefening_2_2.php
+++ Periode-1/T3_REA2/T3_REA_Oefening_2_2.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<!--html blok buiten het php blok -->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,24 +11,31 @@
 
 <body>
     <?php
+        //elementen van html UI in één echo
+
+        //header
         echo '<div class="header">' . 
             "<h1>Uitwerking van PHP-oefeningen</h1>" .
-        "</div>";
+        "</div>" .
 
-        echo "<main>" .
+        //navbar
+        "<main>" .
             '<div class="nav">' .
-                "<h3>Thema 3</h3>" .
+            "<h2>Thema 3</h2>" .
 
             "<ul>" .
                     "<li>" .
-                        "<p> Hoofdstuk 2</p>" .
-                        '<li a href="X">Oefening 2.1</li>' .
-                        '<li a href="x">Oefening 2.2</li>' .
+                        "<h2> Hoofdstuk 2</h2>" .
+                        '<li a href="#">Oefening 2.1</li>' .
+                        '<li a href="#">Oefening 2.2</li>' .
                     "</li>" .
                 "</ul>" .
             "</div>" .
 
+        //inhoud van artikel / paragraaf / tekst
+        //elementen h2, img en p minimaal 1 keer voorkomen.
             '<div class="content">' .
+                '<h2>Uitwerking oefening 2.2</h2>' .
                 '<p>' .
             '</div>' .
         '</main>';
