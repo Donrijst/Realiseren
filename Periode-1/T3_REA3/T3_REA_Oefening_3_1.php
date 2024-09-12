2e45e4ce8cdf0c8171bb54b89c80a0000db2fcb8
Don Kieu <2177651@talnet.nl>
2024-09-12 12:40:57 +0200

    begin taak 2

--- Periode-1/T3_REA3/T3_REA_Oefening_3_1.php
+++ Periode-1/T3_REA3/T3_REA_Oefening_3_1.php
@@ -1,4 +1,5 @@
 <?php
+    //Taak 1 ---------------------------------------------
     //alle onderstreepte woorden uit de tekst in variabelen.\
     
     $type = "Carnaval";
@@ -9,7 +10,15 @@
     $provincie = "Noord-Brabant";
     $voedingsmiddelen = ['worstenbrood', 'koffie Schrobbeler'];
 
+    //tekst met de woorden uit de tekst in één $story variabele.
     $story = "{$type} is van oorsprong een gekerstend heidens volksfeest. Het duurt officieel {$duur} dagen, {$dagen[0]}, {$dagen[1]} en {$dagen[2]} direct voorafgaand aan de vastentijd van {$vastentijd} dagen. {$type} is bij uitstek het feest van zotheid, spot en uitbundigheid. Inmiddels is het in verschillende plaatsen door heel {$land} een gebruik om {$type} op carnavalsvrijdag te openen. Op Aswoensdag wordt {$type} afgesloten, maar de tradities verschillen per regio. In het overgrote deel van {$provincie} wordt er afgesloten met {$voedingsmiddelen[0]}, {$voedingsmiddelen[1]}soms met een koffietafel.";
+
+
+    //Taak 2 --------------------------------------------------
+    //maak voor 3 bonnen variabelen aan
+    $bon1 = 34.99;
+    $bon2 = 45.76;
+    $bon3 = 78.34;
 
 ?>
 
@@ -18,7 +27,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Test der Testen</title>
     <link rel="stylesheet" href="../style.css">
 </head>
 
@@ -45,11 +54,16 @@
             "</ul>" .
         "</div>" .
 
+        //pakt de tekst uit de $story variabele in een p element
         '<div class="content">' .
             '<br>' .
-                '<h2>Uitwerking oefening 2.</h2>' .
+                '<h2>Uitwerking oefening 3.1</h2>' .
             '<br>' .
-            "<p>{$story}</p>" .
+                '<h3>Taak 1--------------</h3>' .
+                "<p>{$story}</p>" .
+            '<br>' .
+                '<h3>Taak 2--------------</h3>' .
+
         "</div>" .
     "</main>";
     ?>
